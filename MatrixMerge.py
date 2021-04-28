b8c6345320ce5dc029780309096a1e0b51fbb31b
GT Symington <gtsrex@gmail.com>
2021-04-28 23:49:32 +0200

    inital commit

---
+++ MatrixMerge.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+A_1 = [0,0]
+A_2 = [0,1]
+A_3 = [0,2]
+
+B_1 = [1,0]
+B_2 = [1,1]
+B_3 = [1,2]
+
+A = np.vstack([A_1, A_2, A_3])
+B = np.vstack([B_1, B_2, B_3])
+
+class MatrixMerge:
+    def __init__(self, A, B):
+        self.A = A
+        self.B = B
+        
+
+    def createMerge(self):
+        self.C = []
+        new_row = []
+        for i, row in enumerate(A, 0):   # gets both index and object at index for legibility
+            for j, element in enumerate(B[i], 0):
+                #print(element)
+                np.append(row, element)
+            self.C.append(row.flatten())
+            print(self.C)
+            new_row.clear()
+        return self.C
+        
+
+if __name__=="__main__":
+    print(A[0])
+    print(MatrixMerge(A=A, B=B).createMerge()) 
