71cabdc97173bae12eb20878b7ae25d74a936d72
GT Symington <gtsrex@gmail.com>
2021-04-29 10:15:10 +0200

    added better documentation

--- MatrixMerge.py
+++ MatrixMerge.py
@@ -30,12 +30,25 @@ class MatrixMerge:
         return self.C
 
     def createMergeTheSequel(self):
-        size = len(self.A)*len(self.B)
+        """
+        desired product: a new matrix of arrays combining the rows of A and B such that calling C[0][1] returns [A[0],B[1]].flatten()
+        - currently works only for C[0][0]
+        """
+        #size = len(self.A)*len(self.B)
         new_row = []
         new_matrix = []
         for i, row in enumerate(A, 0):
             new_row = np.concatenate((row,B[i]))
             new_matrix.append(new_row)
+
+        # create new vstack with the elements in B shuffled
+
+        # add new matrix combo
+
+        # create new vstack with the elements in A shuffled
+
+        # add new matrix combo
+
         return new_matrix        
 
 if __name__=="__main__":
